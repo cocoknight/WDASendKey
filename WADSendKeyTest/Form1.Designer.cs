786f7543b26abb2de8d5657930c16e8d051f340a
cocoknight <cocoknight@naver.com>
2020-06-12 10:07:08 +0900

    06/12/2020 : first release(1.0.0.1)

--- WADSendKeyTest/Form1.Designer.cs
+++ WADSendKeyTest/Form1.Designer.cs
@@ -33,10 +33,10 @@
             this.cmdStart = new System.Windows.Forms.Button();
             this.txtBox_Exception = new System.Windows.Forms.RichTextBox();
             this.grpTestSetting = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtNotePad_num = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtPrint_num = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNotePad_num = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.grpTestResult.SuspendLayout();
             this.grpTestSetting.SuspendLayout();
             this.SuspendLayout();
@@ -44,9 +44,11 @@
             // grpTestResult
             // 
             this.grpTestResult.Controls.Add(this.ResultListView);
-            this.grpTestResult.Location = new System.Drawing.Point(38, 105);
+            this.grpTestResult.Location = new System.Drawing.Point(43, 131);
+            this.grpTestResult.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.grpTestResult.Name = "grpTestResult";
-            this.grpTestResult.Size = new System.Drawing.Size(731, 276);
+            this.grpTestResult.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpTestResult.Size = new System.Drawing.Size(835, 345);
             this.grpTestResult.TabIndex = 0;
             this.grpTestResult.TabStop = false;
             this.grpTestResult.Text = "Test Result";
@@ -54,17 +56,19 @@
             // ResultListView
             // 
             this.ResultListView.HideSelection = false;
-            this.ResultListView.Location = new System.Drawing.Point(31, 20);
+            this.ResultListView.Location = new System.Drawing.Point(35, 25);
+            this.ResultListView.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.ResultListView.Name = "ResultListView";
-            this.ResultListView.Size = new System.Drawing.Size(666, 238);
+            this.ResultListView.Size = new System.Drawing.Size(761, 296);
             this.ResultListView.TabIndex = 0;
             this.ResultListView.UseCompatibleStateImageBehavior = false;
             // 
             // cmdStart
             // 
-            this.cmdStart.Location = new System.Drawing.Point(664, 554);
+            this.cmdStart.Location = new System.Drawing.Point(759, 692);
+            this.cmdStart.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmdStart.Name = "cmdStart";
-            this.cmdStart.Size = new System.Drawing.Size(71, 31);
+            this.cmdStart.Size = new System.Drawing.Size(81, 39);
             this.cmdStart.TabIndex = 1;
             this.cmdStart.Text = "Start";
             this.cmdStart.UseVisualStyleBackColor = true;
@@ -72,9 +76,10 @@
             // 
             // txtBox_Exception
             // 
-            this.txtBox_Exception.Location = new System.Drawing.Point(39, 397);
+            this.txtBox_Exception.Location = new System.Drawing.Point(45, 496);
+            this.txtBox_Exception.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtBox_Exception.Name = "txtBox_Exception";
-            this.txtBox_Exception.Size = new System.Drawing.Size(730, 141);
+            this.txtBox_Exception.Size = new System.Drawing.Size(834, 175);
             this.txtBox_Exception.TabIndex = 2;
             this.txtBox_Exception.Text = "";
             this.txtBox_Exception.TextChanged += new System.EventHandler(this.TxtBox_Exception_TextChanged);
@@ -85,56 +90,61 @@
             this.grpTestSetting.Controls.Add(this.label2);
             this.grpTestSetting.Controls.Add(this.txtNotePad_num);
             this.grpTestSetting.Controls.Add(this.label1);
-            this.grpTestSetting.Location = new System.Drawing.Point(39, 25);
+            this.grpTestSetting.Location = new System.Drawing.Point(45, 31);
+            this.grpTestSetting.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.grpTestSetting.Name = "grpTestSetting";
-            this.grpTestSetting.Size = new System.Drawing.Size(730, 74);
+            this.grpTestSetting.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grpTestSetting.Size = new System.Drawing.Size(834, 92);
             this.grpTestSetting.TabIndex = 3;
             this.grpTestSetting.TabStop = false;
             this.grpTestSetting.Text = "Test Setting";
             // 
-            // label1
+            // txtPrint_num
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Repeat Number of Notepad Running :";
-            // 
-            // txtNotePad_num
-            // 
-            this.txtNotePad_num.Location = new System.Drawing.Point(238, 29);
-            this.txtNotePad_num.Name = "txtNotePad_num";
-            this.txtNotePad_num.Size = new System.Drawing.Size(122, 21);
-            this.txtNotePad_num.TabIndex = 1;
+            this.txtPrint_num.Location = new System.Drawing.Point(674, 36);
+            this.txtPrint_num.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPrint_num.Name = "txtPrint_num";
+            this.txtPrint_num.Size = new System.Drawing.Size(139, 25);
+            this.txtPrint_num.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(404, 32);
+            this.label2.Location = new System.Drawing.Point(462, 40);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 12);
+            this.label2.Size = new System.Drawing.Size(210, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Repeat Number of Print String :";
             // 
-            // txtPrint_num
+            // txtNotePad_num
             // 
-            this.txtPrint_num.Location = new System.Drawing.Point(590, 29);
-            this.txtPrint_num.Name = "txtPrint_num";
-            this.txtPrint_num.Size = new System.Drawing.Size(122, 21);
-            this.txtPrint_num.TabIndex = 3;
+            this.txtNotePad_num.Location = new System.Drawing.Point(272, 36);
+            this.txtNotePad_num.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtNotePad_num.Name = "txtNotePad_num";
+            this.txtNotePad_num.Size = new System.Drawing.Size(139, 25);
+            this.txtNotePad_num.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(251, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Repeat Number of Notepad Running :";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 609);
+            this.ClientSize = new System.Drawing.Size(922, 761);
             this.Controls.Add(this.grpTestSetting);
             this.Controls.Add(this.txtBox_Exception);
             this.Controls.Add(this.cmdStart);
             this.Controls.Add(this.grpTestResult);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "SendKey Verification";
+            this.Text = "SendKey Verification 1.0.0.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.grpTestResult.ResumeLayout(false);
             this.grpTestSetting.ResumeLayout(false);
